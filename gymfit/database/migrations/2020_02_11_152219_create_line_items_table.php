4d82516e0524e45d940b18ada4fdcae00cbabae7
Adry2612 <adrianvidalopez12@gmail.com>
2020-02-11 17:22:43 +0100

    Creacion de migraciones

---
+++ gymfit/database/migrations/2020_02_11_152219_create_line_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLineItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lineItems', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->float('price');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lineItems');
+    }
+}
