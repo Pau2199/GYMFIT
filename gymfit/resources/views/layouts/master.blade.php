fa28b7c4acfde3f5fcdd1060835468301fe28e05
Pau <pau@gmail.com>
2020-02-11 16:08:12 +0100

    terminado vista master

--- gymfit/resources/views/layouts/master.blade.php
+++ gymfit/resources/views/layouts/master.blade.php
@@ -46,105 +46,59 @@
                 </ul>
             </div>
         </nav>
-        <!-- Footer -->
         <footer class="page-footer font-small blue-grey lighten-5 footer">
-
-            <!-- Footer Links -->
-            <div class="container text-center text-md-left mt-5">
-
-                <!-- Grid row -->
-                <div class="row mt-3 dark-text">
-
-                    <!-- Grid column -->
-                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-
-                        <!-- Links -->
-                        <h6 class="text-uppercase font-weight-bold">Products</h6>
-                        <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+            <div class="container-fluid text-center text-md-left">
+                <div class="row mt-3 dark-text mt-5">
+                    <div class="col-md-3 col-lg-2 mx-auto mt-3">
+                        <h6 class="text-uppercase font-weight-bold">Información</h6>
+                        <hr class="mb-4 mt-0 d-inline-block w-50">
                         <p>
-                            <a class="dark-text" href="#!">MDBootstrap</a>
+                            <a class="text-warning" href="#!">¿Quienes Somos?</a>
                         </p>
                         <p>
-                            <a class="dark-text" href="#!">MDWordPress</a>
+                            <a class="text-warning" href="#!">Terminos y condiciones</a>
                         </p>
                         <p>
-                            <a class="dark-text" href="#!">BrandFlow</a>
+                            <a class="text-warning" href="#!">Trabaja con Nosotros</a>
                         </p>
-                        <p>
-                            <a class="dark-text" href="#!">Bootstrap Angular</a>
-                        </p>
-
                     </div>
-                    <!-- Grid column -->
-
-                    <!-- Grid column -->
-                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-
-                        <!-- Links -->
-                        <h6 class="text-uppercase font-weight-bold">Useful links</h6>
-                        <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                    <div class="col-md-3 col-lg-2 mx-auto mt-3">
+                        <h6 class="text-uppercase font-weight-bold">Productos</h6>
+                        <hr class="mb-4 mt-0 d-inline-block w-50">
                         <p>
-                            <a href="#!">Your Account</a>
+                            <a class="text-warning" href="#!">Ropa</a>
                         </p>
                         <p>
-                            <a href="#!">Become an Affiliate</a>
+                            <a class="text-warning" href="#!">Materiales</a>
                         </p>
                         <p>
-                            <a class="dark-grey-text" href="#!">Shipping Rates</a>
+                            <a class="text-warning" href="#!">Maquinas</a>
                         </p>
-                        <p>
-                            <a class="dark-grey-text" href="#!">Help</a>
-                        </p>
-
                     </div>
-                    <!-- Grid column -->
-
-                    <!-- Grid column -->
-                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-
-                        <!-- Links -->
-                        <h6 class="text-uppercase font-weight-bold">Contact</h6>
-                        <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-                        <p>
-                            <i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-                        <p>
-                            <i class="fas fa-envelope mr-3"></i> info@example.com</p>
-                        <p>
-                            <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-                        <p>
-                            <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
-
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 mt-3">
+                        <h6 class="text-uppercase font-weight-bold">Contacta con nosotros!</h6>
+                        <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 215px;">
+                        <p>+34 685 21 22 56</p>
+                        <p>964 55 41 78</p>
+                        <p>contacto@gymfit.com</p>
                     </div>
-                    <!-- Grid column -->
-
                 </div>
-                <!-- Grid row -->
-
             </div>
-            <div style="background-color: #222831;">
-                <div class="container">
-
-                    <!-- Grid row-->
-                    <div class="row py-4 d-flex align-items-center">
-
-                        <!-- Grid column -->
-                        <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
-                            <h6 class="mb-0">Siguenos en nuestras redes Sociales!</h6>
+                <div class="container-fluid fondoCopyEnlaces">
+                    <div class="row py-3 d-flex">
+                        <div class="col-md-6 col-lg-5 text-center mb-4">
+                            <h6 class="mb-0 colorLetra">Siguenos en nuestras redes Sociales!</h6>
                         </div>
-                        <div class="col-md-6 col-lg-7 text-center text-md-right">
+                        <div class="col-md-6 col-lg-7 text-center">
                                 <img class="redesSociales" src="{{asset('img/instagram.png')}}" alt="Instragram">
-                                <img class="redesSociales" src="{{asset('img/twiter.png')}}" alt="Twitter">
+                                <img class="redesSociales" src="{{asset('img/twitter.png')}}" alt="Twitter">
                                 <img class="redesSociales" src="{{asset('img/facebook.png')}}" alt="Facebook">
                         </div>
                     </div>
-                    <div class="footer-copyright text-center text-black-50 py-3">
-                       © 2020 Copyright:
-                        <a class="dark-grey-text" href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
+                    <div class="footer-copyright text-center py-3 colorLetra">
+                       © 2020 Copyright: GYMFIT
                     </div>
-
-
                 </div>
-            </div>
         </footer>
     </body>
 </html>
