e48d098099dc5c7dbce08714e796c2ba010cfc62
Alex <alejandrosal1396@gmail.com>
2020-02-14 17:09:49 +0100

    Terminado vista verProducto

--- gymfit/routes/web.php
+++ gymfit/routes/web.php
@@ -11,42 +11,14 @@
 |
 */
 
-<<<<<<< HEAD
 Route::get('/', function () {
-<<<<<<< HEAD
 
-    return view('agregarProducto');
+    return view('vistaProducto');
 
 });
-
-
 
 Route::get('product/store', 'ProductController@store');
 
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    
-    return view('crudProductos');
-    
-=======
-    return view('index');
->>>>>>> master
-=======
-    return view('vistaProducto');
->>>>>>> pau
-=======
-    
-    return view('crudUsuarios');
-
->>>>>>> Alex
-=======
-Route::get('/', function(){
-    return view('index');
->>>>>>> pau
-});
 Route::get('/agregarProducto', 'ProductosController@store');
 
 
->>>>>>> master
